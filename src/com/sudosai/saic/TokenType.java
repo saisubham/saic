62c32c512f7d64b23919a54b6490dcc3d1b00139
saisubham <subhamsaibehera11@gmail.com>
2022-06-01 14:20:07 +0530

    Built lexical scanner

---
+++ src/com/sudosai/saic/TokenType.java
@@ -0,0 +1,17 @@
+package com.sudosai.saic;
+
+public enum TokenType {
+    // single-char tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one or two char tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
