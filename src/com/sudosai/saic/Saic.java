7c0b8d4dddb27bd308f1f0514f50cb7358855a37
saisubham <subhamsaibehera11@gmail.com>
2022-06-02 22:43:28 +0530

    Built recursive descent parser.

--- src/com/sudosai/saic/Saic.java
+++ src/com/sudosai/saic/Saic.java
@@ -42,10 +42,12 @@ public class Saic {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expr = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expr));
     }
 
     static void error(int line, String message) {
@@ -56,4 +58,13 @@ public class Saic {
         System.out.println("[line: " + line + "] Error: " + where + ": " + message);
         hadError = true;
     }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, "at end", message);
+        } else {
+            report(token.line, "at '" + token.lexeme + "'", message);
+        }
+    }
+
 }
